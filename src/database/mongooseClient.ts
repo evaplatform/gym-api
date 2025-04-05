eb88a3a74dfab99ee8cd7c4f9835e62f797c24fe
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2025-04-05 19:42:57 -0300

    test

--- src/database/mongooseClient.ts
+++ src/database/mongooseClient.ts
@@ -9,13 +9,20 @@ export const MongooseClient = {
   async connect(): Promise<void> {
     if (isConnected) return;
 
-    const uri = process.env.MONGODB_URL || '';
-    const username = process.env.MONGODB_USERNAME || '';
-    const password = process.env.MONGODB_PASSWORD || '';
+    // const uri = process.env.MONGODB_URL || '';
+    // const username = process.env.MONGODB_USERNAME || '';
+    // const password = process.env.MONGODB_PASSWORD || '';
 
-    const connectionString = uri
-      .replace('<username>', username)
-      .replace('<password>', password);
+    // const connectionString = uri
+    //   .replace('<username>', username)
+    //   .replace('<password>', password);
+
+    const username = encodeURIComponent(process.env.MONGODB_USERNAME || '');
+    const password = encodeURIComponent(process.env.MONGODB_PASSWORD || '');
+    const host = process.env.MONGODB_HOST || '';
+    const db = process.env.MONGODB_DB || 'gym-db';
+
+    const connectionString = `mongodb+srv://${username}:${password}@${host}/${db}?retryWrites=true&w=majority`;
 
     try {
       await mongoose.connect(connectionString);
