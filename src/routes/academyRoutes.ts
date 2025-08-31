d0177ea463469c24a6c9be0f7d4d4123d6949a00
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2025-08-31 12:22:42 -0300

    fet: api "get by id" in academy has added

--- src/routes/academyRoutes.ts
+++ src/routes/academyRoutes.ts
@@ -4,6 +4,7 @@ import { AcademyController } from '../controllers/AcademyController';
 const router = express.Router();
 
 router.get('/', AcademyController.getAll);
+router.get('/:id', AcademyController.getById);
 router.post('/', AcademyController.create);
 router.patch('/', AcademyController.update);
 router.delete('/:id', AcademyController.delete);
