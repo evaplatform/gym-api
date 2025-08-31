e36b9a3933f2c87e71bb3b6808236fdf12be862a
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2025-08-30 21:37:21 -0300

    feat: academy properties updatee

--- src/models/academy/IAcademy.ts
+++ src/models/academy/IAcademy.ts
@@ -6,9 +6,9 @@ export interface IAcademy {
   location: string;
   phoneNumber: string;
   imagePath?: string;
-  exerciseIds?: string[]; // point to exercise collection
-  exerciseBlockIds?: string[]; // point to exerciseBlock collection
-  groupIds?: string[]; // point to group collection
+  // exerciseIds?: string[]; // point to exercise collection
+  // exerciseBlockIds?: string[]; // point to exerciseBlock collection
+  // groupIds?: string[]; // point to group collection
   userLimit: number;
   paymentInfo?: IPaymentInfo;
   createdAt: Date;
