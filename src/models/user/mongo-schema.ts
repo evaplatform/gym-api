d799c12a6e2ac5af0069be150c55999b8c91d02b
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2025-09-05 13:17:32 -0300

    feat: set cpf as optional

--- src/models/user/mongo-schema.ts
+++ src/models/user/mongo-schema.ts
@@ -26,7 +26,7 @@ const UserSchema = new mongoose.Schema<IUser>(
     },
     cpf: { 
       type: String, 
-      required: true, 
+      required: false, 
       unique: true, 
       maxlength: 14, 
       minlength: 14, 
