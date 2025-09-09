0b111daf36e6147c6149fd0aa0c69a4f81f0cb95
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2025-09-09 12:14:46 -0300

    aliases file updated

--- src/config/aliases.ts
+++ src/config/aliases.ts
@@ -1,5 +1,10 @@
+// src/config/moduleAlias.ts
+import path from 'path';
 import moduleAlias from 'module-alias';
 
+// Configurar aliases baseado no ambiente
+const baseDir = path.resolve(__dirname, '../');
+
 moduleAlias.addAliases({
-  '@': __dirname + '/..'
+  '@': baseDir
 });
