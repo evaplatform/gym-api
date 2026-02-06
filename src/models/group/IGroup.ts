394fedac8383adbf0e214c098a3b4cb78b9e60ac
murilodevkapaz <70186237+murilodevkapaz@users.noreply.github.com>
2026-02-06 09:27:10 -0300

    groups interface updated

--- src/models/group/IGroup.ts
+++ src/models/group/IGroup.ts
@@ -1,12 +1,73 @@
 import { IdType } from "@/shared/types/IdType";
 import { IDefaultEntityProperties } from "../../shared/interfaces/IDefaultEntityProperties";
-
+export interface PermissionNode {
+  permitted: boolean;
+}
 export interface IGroup extends IDefaultEntityProperties {
   name: string; // unique
   academyId: IdType; // point to academy collection
   permissions: {
+    changeAcademy: {
+      permitted: boolean;
+    }
     drawerMenu: {
-      drawer: boolean;
+      permitted: boolean;
+      home?: {
+        permitted: boolean;
+        tabs?: {
+          home: PermissionNode;
+          calendar?: PermissionNode;
+          exercises?: {
+            permitted: boolean;
+            finalizeTrainingButton: PermissionNode;
+            finalizeExerciseButton: PermissionNode;
+            userGpsButton: PermissionNode;
+          };
+          cardio?: PermissionNode;
+          financial?: PermissionNode;
+        };
+      };
+      users?: {
+        permitted: boolean;
+        add: PermissionNode;
+        delete: PermissionNode;
+        update: PermissionNode;
+      };
+      academies?: {
+        permitted: boolean;
+        add: PermissionNode;
+        delete: PermissionNode;
+        update: PermissionNode;
+      };
+      exercises?: {
+        permitted: boolean;
+        add: PermissionNode;
+        delete: PermissionNode;
+        update: PermissionNode;
+      };
+      trainings?: {
+        permitted: boolean;
+        add: PermissionNode;
+        delete: PermissionNode;
+        update: PermissionNode;
+      };
+      userSettings?: {
+        permitted: boolean;
+        resetDataButton: PermissionNode;
+      };
+      charts?: {
+        permitted: boolean;
+        deleteHistoryButton: PermissionNode;
+        deleteAllHistoryButton: PermissionNode;
+      };
+      groups?: {
+        changeAcademyButton: PermissionNode;
+        permitted: boolean;
+        add: PermissionNode;
+        delete: PermissionNode;
+        update: PermissionNode;
+      };
     };
   };
 }
+
--- src/models/group/mongo-schema.ts
+++ src/models/group/mongo-schema.ts
@@ -7,7 +7,7 @@ const GroupSchema = new mongoose.Schema<IGroup>(
     name: {
       type: String,
       required: true,
-      unique: false,
+      unique: true,
       maxlength: 100,
       description: 'The name of the group, must be unique and up to 100 characters long.'
     },
@@ -18,14 +18,76 @@ const GroupSchema = new mongoose.Schema<IGroup>(
       description: 'The ID of the academy this group belongs to'
     },
     permissions: {
-      drawerMenu: {
-        drawer: {
-          type: Boolean,
-          default: false,
-          description: 'Indicates whether the drawer menu is accessible.'
-        },
+      changeAcademy: {
+        permitted: { type: Boolean, default: false }
       },
-    },
+      drawerMenu: {
+        permitted: { type: Boolean, default: false },
+        home: {
+          permitted: { type: Boolean, default: false },
+          tabs: {
+            home: {
+              permitted: { type: Boolean, default: false }
+            },
+            calendar: {
+              permitted: { type: Boolean, default: false }
+            },
+            exercises: {
+              permitted: { type: Boolean, default: false },
+              finalizeTrainingButton: { permitted: { type: Boolean, default: false } },
+              finalizeExerciseButton: { permitted: { type: Boolean, default: false } },
+              userGpsButton: { permitted: { type: Boolean, default: false } }
+            },
+            cardio: {
+              permitted: { type: Boolean, default: false }
+            },
+            financial: {
+              permitted: { type: Boolean, default: false }
+            }
+          }
+        },
+        users: {
+          permitted: { type: Boolean, default: false },
+          add: { permitted: { type: Boolean, default: false } },
+          delete: { permitted: { type: Boolean, default: false } },
+          update: { permitted: { type: Boolean, default: false } }
+        },
+        academies: {
+          permitted: { type: Boolean, default: false },
+          add: { permitted: { type: Boolean, default: false } },
+          delete: { permitted: { type: Boolean, default: false } },
+          update: { permitted: { type: Boolean, default: false } }
+        },
+        exercises: {
+          permitted: { type: Boolean, default: false },
+          add: { permitted: { type: Boolean, default: false } },
+          delete: { permitted: { type: Boolean, default: false } },
+          update: { permitted: { type: Boolean, default: false } }
+        },
+        trainings: {
+          permitted: { type: Boolean, default: false },
+          add: { permitted: { type: Boolean, default: false } },
+          delete: { permitted: { type: Boolean, default: false } },
+          update: { permitted: { type: Boolean, default: false } }
+        },
+        userSettings: {
+          permitted: { type: Boolean, default: false },
+          resetDataButton: { permitted: { type: Boolean, default: false } }
+        },
+        charts: {
+          permitted: { type: Boolean, default: false },
+          deleteHistoryButton: { permitted: { type: Boolean, default: false } },
+          deleteAllHistoryButton: { permitted: { type: Boolean, default: false } }
+        },
+        groups: {
+          permitted: { type: Boolean, default: false },
+          changeAcademyButton: { permitted: { type: Boolean, default: false } },
+          add: { permitted: { type: Boolean, default: false } },
+          delete: { permitted: { type: Boolean, default: false } },
+          update: { permitted: { type: Boolean, default: false } }
+        }
+      }
+    }
   }, { ...MONGO_DEFAULT_PROPERTIES });
 
 export const GroupModel = mongoose.model<IGroup>('Group', GroupSchema);
